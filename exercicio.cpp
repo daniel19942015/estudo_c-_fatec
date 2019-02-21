53ae97afc1de4aec9624a9908ea7cbac802f8bd8
SunriseSystem <daniel199257@gmail.com>
2019-02-21 20:38:55 -0300

    Add files via upload

---
+++ exercicio.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+
+using namespace std;
+
+int main(){
+    system("cls");
+    setlocale(LC_ALL, "Portuguese");
+    
+    
+    
+    
+    
+    system("pause");
+    return 0;
+
+}

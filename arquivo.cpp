da3d4933684c3bd345c32ee890c6859cb1a256a5
Daniel Silva <daniel199257@gmail.com>
2019-02-20 22:22:24 -0300

    Atualizacao

--- arquivo.cpp
+++ arquivo.cpp
@@ -1,1 +1,18 @@
-arquivo.cpp
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+
+    setlocale(LC_ALL, "Portuguese");
+
+    int resultado = 0;
+
+    cin >> resultado;
+
+    
+    return 0; 
+}
